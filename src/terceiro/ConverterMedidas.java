cd401dca06dfe5888f61b974c151ae46607f5535
GabrielSilva88 <gabrielmg.silva@outlook.com>
2024-08-18 15:28:57 -0300

    update exercicios condicionais.

--- src/terceiro/ConverterMedidas.java
+++ src/terceiro/ConverterMedidas.java
@@ -1,6 +1,17 @@
+/* 
+ Desenvolva um conversor que recebe uma unidade de medida (metro, centímetro, quilômetro) 
+ e converte para outra unidade com base na entrada. 
+    
+*/
 package terceiro;
 
+import java.util.Scanner;
+
 public class ConverterMedidas {
-	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		
+	}
 	
 }
